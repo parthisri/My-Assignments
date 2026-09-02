5bb5559398905dd260657fbd942ff8d34a02e6c0
parthisri <parthisri94@gmail.com>
2026-09-02 19:08:18 +0530

    Add files via upload

---
+++ dataType.js
@@ -0,0 +1,19 @@
+var firstName = "Parthiban";
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName = "RFPI0";
+console.log(companyName);
+console.log(typeof companyName);
+
+var mobileNumber = "9876543210";
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+var isAutomation = true;
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+var hasPlaywright;
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
